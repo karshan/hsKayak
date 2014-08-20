da8964d3de4968786a1e11beb23699e81ae5e52e
Karshan Sharma <karshan.sharma@gmail.com>
2014-08-19 17:28:28 -0700

    cleanup

--- Main.hs
+++ Main.hs
@@ -1,93 +1,78 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Control.Applicative ((<$>))
 import Control.Lens ((&), (.~), (^.))
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.State (StateT)
-import qualified Control.Monad.Trans.State as S (get, put)
 import qualified Data.ByteString.Char8 as BS (ByteString, pack)
 import qualified Data.ByteString.Lazy.Char8 as LBS (ByteString, unpack)
 import Data.Maybe (fromMaybe, mapMaybe, listToMaybe)
-import Data.Monoid ((<>))
-import Data.List (isPrefixOf)
+import Data.List (find, isPrefixOf)
 import Network.Connection (TLSSettings(..))
-import Network.HTTP.Client (CookieJar)
 import Network.HTTP.Client.TLS (mkManagerSettings)
-import Network.Wreq (Options, Response, postWith, getWith, proxy, manager, httpProxy, responseCookieJar, responseBody, cookies)
+import Network.Wreq (Options, postWith, getWith, proxy, manager, httpProxy, responseBody)
 import qualified Network.Wreq as W (FormParam((:=)), defaults)
 import Network.Wreq.Types (Postable)
-import Text.XML.HXT.Core
 import Data.Tree.NTree.TypeDefs (NTree)
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import Text.XML.Light
+import Text.XML.HXT.Core
 
+-- Utils
 (^=) :: BS.ByteString -> BS.ByteString -> W.FormParam
 (^=) = (W.:=)
 
-defaults :: Options
-defaults = W.defaults & proxy .~ httpProxy "localhost" 8080 & manager .~ Left (mkManagerSettings (TLSSettingsSimple True False False) Nothing)
+defOpts :: Options
+defOpts = W.defaults
+
+proxyDefaults :: Options
+proxyDefaults = W.defaults & proxy .~ httpProxy "localhost" 8080 & manager .~ Left (mkManagerSettings (TLSSettingsSimple True False False) Nothing)
 
 post :: (Postable a) => String -> a -> IO LBS.ByteString
-post url params = (^. responseBody) `fmap` postWith defaults url params
+post url params = (^. responseBody) `fmap` postWith defOpts url params
 
 get :: String -> IO LBS.ByteString
-get url = (^. responseBody) `fmap` getWith defaults url
+get url = (^. responseBody) `fmap` getWith defOpts url
 
 css :: ArrowXml a => String -> a (NTree XNode) XmlTree
 css tag = multi (hasName tag)
 
-getPrice :: String -> String -> String -> String -> IO (Maybe [String])
-getPrice orig dest dep ret = do
-    originsid <- getOriginsId `fmap` get ("http://www.kayak.com/flights/" ++ orig ++ "-" ++ dest ++ "/" ++ dep ++ "/" ++ ret)
-    now <- round `fmap` getPOSIXTime :: IO Int
-    results <- post ("http://www.kayak.com/s/jspoll?ss=1&poll=1&final=false&updateStamp=" ++ show now) [ "lmname" ^= ""
-                                                                                                       , "lmname2" ^= ""
-                                                                                                       , "c" ^= "15" -- This choses how many results
-                                                                                                       , "s" ^= "price"
-                                                                                                       , "searchid" ^= originsid
-                                                                                                       , "itd" ^= ""
-                                                                                                       , "poll" ^= "1"
-                                                                                                       , "seo" ^= "false"
-                                                                                                       , "vw" ^= "list"
-                                                                                                       , "urlViewState" ^= ""
-                                                                                                       , "streaming" ^= "false"
-                                                                                                       ]
-    return $ getIteneraries results
-
+safeHead :: [a] -> Maybe a
 safeHead = listToMaybe
 
-contentDiv :: [Content] -> Maybe Element
-contentDiv = safeHead . mapMaybe (filterElement (myhasAttr' "id" (=="content_div"))) . onlyElems
+toQName :: String -> Text.XML.Light.QName
+toQName s = QName s Nothing Nothing
 
-resultRowToLegdatas :: Element -> [String]
-resultRowToLegdatas e = map mygetText $ filterElements (myhasAttr' "id" ("legdata" `isPrefixOf`)) e
+hasAttrVal :: String -> String -> Element -> Bool
+hasAttrVal k v e = findAttr (toQName k) e == Just v
 
-mygetText :: Element -> String
-mygetText e = fromMaybe "mygetText Failed" (do
-    c <- safeHead $ elContent e
-    d <- safeHead $ onlyText $ return c
-    return $ cdData d)
+hasAttrValBy :: String -> (String -> Bool) -> Element -> Bool
+hasAttrValBy k p e = maybe False p $ findAttr (toQName k) e
+-- End Utils
 
-resultRowToPrice :: Element -> String
-resultRowToPrice = maybe "resultRowToPrice Failed" mygetText . safeHead . filterElements (myhasAttr' "class" (=="results_price bookitprice"))
+data Itenerary = Itenerary { price :: String, legs :: [String] } deriving Show
 
-toq :: String -> Text.XML.Light.QName
-toq s = QName s Nothing Nothing
+resultRowToItenerary :: Element -> Maybe Itenerary
+resultRowToItenerary r = do
+    price' <- strContent <$> filterElement (hasAttrVal "class" "results_price bookitprice") r
+    let legs' = map strContent $ filterElements (hasAttrValBy "id" ("legdata" `isPrefixOf`)) r
+    return $ Itenerary price' legs'
 
-myhasAttr' :: String -> (String -> Bool) -> Element -> Bool
-myhasAttr' k f e = Just True == f `fmap` lookup (toq k) (map (attrKey &&& attrVal) $ elAttribs e)
+getIteneraries :: String -> Maybe [Itenerary]
+getIteneraries x = do
+    c <- find (hasAttrVal "id" "content_div") $ onlyElems $ parseXML x
+    let resultRows = elChildren c
+    return $ mapMaybe resultRowToItenerary resultRows
 
-getIteneraries :: LBS.ByteString -> Maybe [String]
-getIteneraries = f . LBS.unpack
-    where
-        f x = do
-            c <- contentDiv $ parseXML x
-            return $ map (show . (resultRowToPrice &&& resultRowToLegdatas)) $ elChildren c
+getOriginsId :: String -> String
+getOriginsId = fromMaybe (error "getOriginsId") . (=<<) (findAttr (toQName "value")) . safeHead . mapMaybe (filterElement (hasAttrVal "name" "originsid")) . onlyElems . parseXML
 
-getOriginsId :: LBS.ByteString -> BS.ByteString
-getOriginsId = BS.pack . f . LBS.unpack
-    where
-        f = head . runLA (hread >>> css "input" >>> hasAttrValue "name" (== "originsid") >>> getAttrValue "value")
+getPrice :: String -> String -> String -> String -> IO (Maybe [Itenerary])
+getPrice orig dest dep ret = do
+    originsid <- (BS.pack . getOriginsId . LBS.unpack) `fmap` get ("http://www.kayak.com/flights/" ++ orig ++ "-" ++ dest ++ "/" ++ dep ++ "/" ++ ret)
+    now <- round `fmap` getPOSIXTime :: IO Int
+    results <- post ("http://www.kayak.com/s/jspoll?ss=1&poll=1&final=false&updateStamp=" ++ show now)
+                [ "lmname" ^= "", "lmname2" ^= "", "c" ^= "15", "s" ^= "price", "searchid" ^= originsid, "itd" ^= "", "poll" ^= "1", "seo" ^= "false", "vw" ^= "list", "urlViewState" ^= "", "streaming" ^= "false"]
+    return $ getIteneraries $ LBS.unpack results
 
 main :: IO ()
 main = do
-    iteneraries <- getPrice "IAD" "YYZ" "2014-08-29" "2014-09-01" 
-    putStr $ maybe "getPrice failed" unlines iteneraries
+    iteneraries <- getPrice "SJC" "ORD" "2014-08-30" "2014-09-01" 
+    print iteneraries
